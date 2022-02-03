7e8990ba9af888f9f85a412854aafcb79662acc8
yschristian <yubahwesc@gmail.com>
2022-02-03 10:41:32 -0800

    come

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 const Home =()=>{
+      const [blogs , setBlog]=useState([
+            {title:'my new bblog',body:'lorem ipsum....',author:'chris',id:1 },
+            {title:'my new bblog12',body:'lorem ipsum....',author:'shema',id:2 },
+            {title:'my new bblog13',body:'lorem ipsum....',author:'yub',id:3 }
+        ])
+
+
     return(
+          
          <div className="home">
-             <h1>Home page</h1>
-            
+              <BlogList blogs={blogs} title="All Blogs"/>
          </div>
     );
 }
