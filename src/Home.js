2eae1605cdf92603a0899604df5fa2ba616e3982
yschristian <yubahwesc@gmail.com>
2022-02-02 17:14:13 -0800

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import {useState} from 'react';
+const Home =()=>{
+    return(
+         <div className="home">
+             <h1>Home page</h1>
+            
+         </div>
+    );
+}
+export default  Home;
