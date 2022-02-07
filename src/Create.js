230f99a84e1e8a1a249b35ede5783a44bd621d73
yschristian <yubahwesc@gmail.com>
2022-02-07 15:55:01 -0800

    form handler

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,42 @@
+import { useState } from "react";
 const Create = ()=>{
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor]=useState('yoshi')
+  const  handleSubmit  = (e)=>{
+       e.preventDefault();
+       const blog =[title,body,author]
+       console.log(blog)
+  }
+
   return(
      <div className="create">
          <h2>Add New Blog</h2>
+         <form onSubmit={handleSubmit}>
+            <label>Blog Title:</label>
+            <input
+             type="text"
+             required
+             value={title}
+             onChange={(e)=>setTitle(e.target.value)}
+             />
+            <label>Blog Body:</label>
+             <textarea
+              required
+              value={body}
+              onChange={(e)=>setBody(e.target.value)}
+             ></textarea>
+             <label>Blog author:</label>
+             <select
+             value={author}
+             onChange={(e)=>setAuthor(e.target.value)}
+             >
+                <option  value="mario">mario</option>
+                <option  value="yoshi">yoshi</option>
+             </select>
+             <button>Add Blog</button>
+             
+         </form>
      </div>
   );
 }
